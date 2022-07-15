eef63e8f0a8702c08a2cbbd0100d1f08d76dc86d
ycavodeassi <yamil.cavodeassi@syspro.com.ar>
2022-07-15 18:03:55 -0300

    Add project files.

---
+++ DepofisFED/DepofisFEDForm.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DepofisFEDForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DepofisFEDForm))
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 25)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(500, 25)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Black", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label1.Location = New System.Drawing.Point(-1, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "DepofisFED"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Button1.Location = New System.Drawing.Point(400, -1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 25)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Si"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.Button2.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Button2.Location = New System.Drawing.Point(450, -1)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 25)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "No"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DepofisFEDForm
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(500, 50)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "DepofisFEDForm"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ DepofisFED/DepofisFEDForm.vb
@@ -0,0 +1,133 @@
+﻿Imports System.Net
+
+Public Class DepofisFEDForm
+    'args
+    Private args() As String = Environment.GetCommandLineArgs()
+    Private _timeFormat As String = "yyyyMMdd_HHmmss"
+    Private _feUri As String
+    Private _fePath As String
+    Private _feNum As String
+    Private _feName As String
+    Private _myDate As String
+    'msg
+    Public Shared Msg, Style, Title, Response
+    'properties
+    Public Property feUri As String
+        Get
+            Return _feUri
+        End Get
+        Set(ByVal valor As String)
+            _feUri = valor
+        End Set
+    End Property
+    Public Property fePath As String
+        Get
+            Return _fePath
+        End Get
+        Set(ByVal valor As String)
+            _fePath = valor
+        End Set
+    End Property
+    Public Property feNum As String
+        Get
+            Return _feNum
+        End Get
+        Set(ByVal valor As String)
+            _feNum = valor
+        End Set
+    End Property
+    Public Property feName As String
+        Get
+            Return _feName
+        End Get
+        Set(ByVal valor As String)
+            _feName = valor
+        End Set
+    End Property
+    Public Property myDate As String
+        Get
+            Return _myDate
+        End Get
+        Set(ByVal valor As String)
+            _myDate = valor
+        End Set
+    End Property
+    Public Property timeFormat As String
+        Get
+            Return _timeFormat
+        End Get
+        Set(ByVal valor As String)
+            _timeFormat = valor
+        End Set
+    End Property
+    ''' <summary>
+    ''' .
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            'args checker
+            If args.Length = 1 Then
+                Throw New System.Exception("Error al recibir uno o mas parametros.")
+            End If
+            'setters
+            Me.myDate = DateTime.Now.ToString(Me.timeFormat)
+            Me.feUri = args(1).Trim
+            Me.fePath = args(2).Trim
+            Me.feNum = args(3).Trim
+            Me.feName = Me.feNum & "_" & Me.myDate & ".PDF"
+            Me.Label1.Text = "Descargando comprobante en linea, por favor espere..."
+            'set download config
+
+            Dim feDownload As New WebClient
+
+            ServicePointManager.Expect100Continue = True
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+
+            'download file
+            AddHandler feDownload.DownloadProgressChanged, AddressOf DownloadProgressChanged
+            AddHandler feDownload.DownloadFileCompleted, AddressOf DownloadFileCompleted
+
+            feDownload.DownloadFileAsync(New System.Uri(Me.feUri), Me.fePath & "\" & feName)
+
+        Catch exToLoadArgs As Exception
+            Msg = exToLoadArgs.Message
+            Style = vbCritical
+            Title = "Error DepofisFED"
+            Response = MsgBox(Msg, Style, Title)
+        End Try
+    End Sub
+    ''' <summary>
+    ''' .
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'yes
+        Process.Start(Me.fePath & "\" & Me.feName)
+    End Sub
+    ''' <summary>
+    ''' .
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'no
+        Me.Close()
+    End Sub
+    ''' <summary>
+    ''' .
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        Me.ProgressBar1.Value = e.ProgressPercentage
+        Me.Label1.Text = e.BytesReceived & "/" & e.TotalBytesToReceive
+    End Sub
+
+    ''' <summary>
+    ''' .
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub DownloadFileCompleted(sender As Object, e As EventArgs)
+        Me.Label1.Text = "Se descargo un comprobante electrónico. ¿Desea abrirlo?"
+    End Sub
+
+End Class
